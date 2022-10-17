0be6e001fc5d2787defa71a81d0a8cb15b4f0400
Thiago dos Santos <thiago-dosantos@outlook.com>
2022-10-17 13:37:18 -0300

    New Content
    new content and hidden about me

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// hidden page
+
+//function openModal() {
+  const modalhidden = document.getElementById('hidden-page')
+  modalhidden.classList.add('openhidden')
+
+  modalhidden.addEventListener('click', (e) => {
+    if(e.target.id == '' || e.target.id == 'hidden-page'){
+      modalhidden.classList.remove('openhidden')
+    }
+  })
+//}
